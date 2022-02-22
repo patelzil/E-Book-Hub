0b4d3d1c288c09e40631ad5e3ae30a07886e9f8d
Patelzil <58107277+Patelzil@users.noreply.github.com>
2022-02-22 02:55:13 -0600

    frontend skeleton created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/home.component"
+import Login from "./components/login.component";
+import Signup from "./components/signup.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <h1>Welcome to the E-book-hub home page</h1>
+        <Routes>
+            <Route path="/" exact element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+        </Routes>
+      </Router>
   );
 }
 
