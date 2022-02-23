2c5dcab6ce82c1e866380d872aa9f35f0e458950
Patelzil <58107277+Patelzil@users.noreply.github.com>
2022-02-22 23:15:37 -0600

    Added navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from "./components/home.component"
 import Login from "./components/login.component";
 import Signup from "./components/signup.component";
+import NavBar from "./components/navbar.component";
 
 function App() {
   return (
       <Router>
-          <h1>Welcome to the E-book-hub home page</h1>
+        <div style={{ zIndex: 1000, top: 0, position: 'sticky', background: 'black' }}><NavBar/></div>
+          {/*<NavBar/>*/}
         <Routes>
-            <Route path="/" exact element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/signup" element={<Signup/>}/>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </Router>
   );
