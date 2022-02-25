251414e706c16acd17bf2b22a12ea29b38e209e5
Patelzil <58107277+Patelzil@users.noreply.github.com>
2022-02-25 02:33:43 -0600

    Error handling on form input

--- frontend/src/components/signup.component.js
+++ frontend/src/components/signup.component.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Button, Form, InputGroup} from "react-bootstrap";
+import {Button, Form} from "react-bootstrap";
 import Modal from 'react-bootstrap/Modal'
 import axios from "axios";
 import online_reading from '../assets/online_reading.svg'
@@ -7,96 +7,121 @@ import online_reading from '../assets/online_reading.svg'
 export default function Signup() {
     const initialUser = {firstName: "", lastName: "", eMail: "", username: "", password: ""}
     const [user, setUser] = useState(initialUser)
-    const [validated, setValidated] = useState(false);
     const [conPass, setConPass] = useState("")
-
+    const [signedUp,setSignedUp] = useState(0);
 
     const handleSubmit = (event) => {
-        event.preventDefault();
         setUser(initialUser);
-        setValidated(true);
-        console.log(user)
-
-        axios.post("http://localhost:5000/EBookHub/users/", user)
-            .then(function(response){
-                if(response.data.status === "sucess"){
-                    alert("successful POST")
-                } else {
-                    alert("failed POST")
-                    console.log(response)
-                }
-            })
-            .catch(function (error) {
-                console.log(error)
-            })
+        console.log(user);
+        if (user.password !== conPass){
+            window.alert("Passwords do not match!");
+        }  else {
+            axios.post("http://localhost:5000/EBookHub/users/createUser", user)
+                .then(function(response){
+                    if(response.data.status === "success"){
+                         setSignedUp(1);
+                    } else {
+                         setSignedUp(2);
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+        }
     }
 
     return (
-        <div className="signup-container">
-            {/*img source: https://undraw.co/search*/}
-            <img className="w-50" style={{maxWidth: "800px", margin: "5%"}} src={online_reading} alt=""/>
-            <Modal.Dialog>
-                <Modal.Header style={{justifyContent: "center"}}>
-                    <Modal.Title >Sign Up</Modal.Title>
-                </Modal.Header>
-                <Modal.Body style={{padding: "20px"}}>
-                    <div className="form-div">
-                        <Form
-                            noValidate
-                            validated={validated}
-                            className="form"
-                            onSubmit={handleSubmit}
-                        >
-                            <Form.Group className="mb-3 input-div" controlId="formFirstName">
-                                <Form.Label>First name</Form.Label>
-                                <Form.Control required type="string" placeholder="Enter first name" onChange={(e) => (initialUser.firstName = e.target.value)}/>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3 input-div" controlId="formLastName">
-                                <Form.Label>Last name</Form.Label>
-                                <Form.Control required type="string" placeholder="Enter last name" onChange={(e) => (initialUser.lastName = e.target.value)}/>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3 input-div" controlId="formEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control required type="email" placeholder="Enter email" onChange={(e) => (initialUser.eMail = e.target.value)}/>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3 input-div" controlId="formUserName">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control required type="string" placeholder="Enter username" onChange={(e) => (initialUser.username = e.target.value)}/>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3 input-div" controlId="formPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control required type="password" placeholder="Password" onChange={(e) => {initialUser.password = e.target.value; }}/>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3 input-div" controlId="formConPassword">
-                                <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control required type="password" placeholder=" Confirm Password" onChange={(e) => setConPass(e.target.value)}/>
-                                <Form.Control.Feedback type="invalid">
-                                    Passwords do not match
-                                </Form.Control.Feedback>
-                            </Form.Group>
-
-                            <Button
-                                className="submit-button input-div"
-                                style={{width: "350px"}}
-                                variant="primary"
-                                type="submit"
-                            >
-                                Submit
-                            </Button>
-
-                            <div className="text-center mt-3">
-                                Already have an account?<br/><a href="/login">Log in</a>
-                            </div>
-                        </Form>
+        <div>
+            { signedUp === 1 ?
+                (<div>
+                    <Modal.Dialog style={{marginTop: "300px", width: "500px", borderColor: "green"}}>
+                        <Modal.Header style={{justifyContent: "center"}}>
+                            <Modal.Title >Sign Up</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body >
+                            <p style={{fontSize: "25px", textAlign: "center"}}>Successfully created an account!</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button className="submit-button" href="/">Close</Button>
+                        </Modal.Footer>
+                    </Modal.Dialog>
+                </div>
+                ): (signedUp === 2) ?
+                (
+                    <div>
+                        <Modal.Dialog style={{marginTop: "300px", width: "500px"}}>
+                            <Modal.Header style={{justifyContent: "center"}}>
+                                <Modal.Title>Error Signing Up</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <p style={{fontSize: "25px",textAlign: "center"}}>Could not sign up. Please try again.</p>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button className="submit-button" variant="black" href="/signup">Close</Button>
+                            </Modal.Footer>
+                        </Modal.Dialog>
                     </div>
-                </Modal.Body>
-            </Modal.Dialog>
-        </div>
+                ) :
+                (<div className="signup-container">
+                    {/*img source: https://undraw.co/search*/}
+                    <img className="w-50" style={{maxWidth: "800px", margin: "5%"}} src={online_reading} alt=""/>
+                    <Modal.Dialog>
+                        <Modal.Header style={{justifyContent: "center"}}>
+                            <Modal.Title >Sign Up</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body style={{padding: "20px"}}>
+                            <div className="form-div">
+                                <Form
+                                    className="form"
+                                    onSubmit={handleSubmit}
+                                >
+                                    <Form.Group className="mb-3 input-div" controlId="formFirstName">
+                                        <Form.Label>First name</Form.Label>
+                                        <Form.Control required type="string" placeholder="Enter first name" onChange={(e) => (initialUser.firstName = e.target.value)}/>
+                                    </Form.Group>
 
+                                    <Form.Group className="mb-3 input-div" controlId="formLastName">
+                                        <Form.Label>Last name</Form.Label>
+                                        <Form.Control required type="string"  placeholder="Enter last name" onChange={(e) => (initialUser.lastName = e.target.value.trim())}/>
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3 input-div" controlId="formEmail">
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control required type="email" placeholder="Enter email" onChange={(e) => (initialUser.eMail = e.target.value.trim())}/>
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3 input-div" controlId="formUserName">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control required type="string"  placeholder="Enter username" onChange={(e) => (initialUser.username = e.target.value.trim())}/>
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3 input-div" controlId="formPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control required type="password" placeholder="Enter password" onChange={(e) => {initialUser.password = e.target.value.trim(); }}/>
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3 input-div" controlId="formConPassword">
+                                        <Form.Label>Confirm Password</Form.Label>
+                                        <Form.Control required type="password" placeholder="Re-enter the password" onChange={(e) => setConPass(e.target.value.trim())}/>
+                                    </Form.Group>
+
+                                    <Button
+                                        className="submit-button input-div"
+                                        style={{width: "350px"}}
+                                        variant="primary"
+                                        type="submit"
+                                    >
+                                        Submit
+                                    </Button>
+
+                                    <div className="text-center mt-3">
+                                        Already have an account?<br/><a href="/login">Log in</a>
+                                    </div>
+                                </Form>
+                            </div>
+                        </Modal.Body>
+                    </Modal.Dialog>
+                </div>)}
+        </div>
     )
 }
