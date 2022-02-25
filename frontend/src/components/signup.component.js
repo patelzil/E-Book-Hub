09b0e82516b53e00aadcb3b72967f3f84b8bc7e8
Patelzil <58107277+Patelzil@users.noreply.github.com>
2022-02-24 18:38:37 -0600

    create user account and add it to database

--- frontend/src/components/signup.component.js
+++ frontend/src/components/signup.component.js
@@ -1,58 +1,39 @@
 import React, {useState} from "react";
-import {Button, Form} from "react-bootstrap";
+import {Button, Form, InputGroup} from "react-bootstrap";
 import Modal from 'react-bootstrap/Modal'
 import axios from "axios";
 import online_reading from '../assets/online_reading.svg'
 
 export default function Signup() {
-    const initialUser = {
-        firstName: "",
-        lastName: "",
-        eMail: "",
-        username: "",
-        password: ""
-    }
-
+    const initialUser = {firstName: "", lastName: "", eMail: "", username: "", password: ""}
     const [user, setUser] = useState(initialUser)
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
-    const [conPassword, setConPassword] = useState('');
+    const [validated, setValidated] = useState(false);
+    const [conPass, setConPass] = useState("")
 
-    const isSignUp = false;
-    // const [isSignUp, setData] = useState(false);
-    // const testData = {
-    //     username: "test",
-    //     password: "test123"
-    // }
 
-    const handleSubmit = () => {
-        // setData(true);
-        // axios.get('http://localhost:5000/Ebookhub/users/Zeeelvaliya')
-        //     .then(res => {
-        //         // const content = res.data
-        //         console.log(res.data.data)
-        //     })
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setUser(initialUser);
+        setValidated(true);
+        console.log(user)
 
-        // axios.post("http://localhost:5000/EBookHub/users/createUser", testData)
-        //     .then(function(response){
-        //         if(response.data.status === "sucess"){
-        //             alert("successful POST")
-        //         } else {
-        //             alert("failed POST")
-        //             console.log(response)
-        //         }
-        //     })
-        //     .catch(function (error) {
-        //         console.log(error)
-        //     })
-
+        axios.post("http://localhost:5000/EBookHub/users/", user)
+            .then(function(response){
+                if(response.data.status === "sucess"){
+                    alert("successful POST")
+                } else {
+                    alert("failed POST")
+                    console.log(response)
+                }
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
     }
 
     return (
         <div className="signup-container">
+            {/*img source: https://undraw.co/search*/}
             <img className="w-50" style={{maxWidth: "800px", margin: "5%"}} src={online_reading} alt=""/>
             <Modal.Dialog>
                 <Modal.Header style={{justifyContent: "center"}}>
@@ -60,36 +41,43 @@ export default function Signup() {
                 </Modal.Header>
                 <Modal.Body style={{padding: "20px"}}>
                     <div className="form-div">
-                        <Form className="form">
+                        <Form
+                            noValidate
+                            validated={validated}
+                            className="form"
+                            onSubmit={handleSubmit}
+                        >
                             <Form.Group className="mb-3 input-div" controlId="formFirstName">
                                 <Form.Label>First name</Form.Label>
-                                <Form.Control type="string" placeholder="Enter first name" onChange={(e) => setFirstName(e.target.value)}
-                                />
+                                <Form.Control required type="string" placeholder="Enter first name" onChange={(e) => (initialUser.firstName = e.target.value)}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3 input-div" controlId="formLastName">
                                 <Form.Label>Last name</Form.Label>
-                                <Form.Control type="string" placeholder="Enter last name" onChange={(e) => setLastName(e.target.value)}/>
+                                <Form.Control required type="string" placeholder="Enter last name" onChange={(e) => (initialUser.lastName = e.target.value)}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3 input-div" controlId="formEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Control required type="email" placeholder="Enter email" onChange={(e) => (initialUser.eMail = e.target.value)}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3 input-div" controlId="formUserName">
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control type="string" placeholder="Enter username" />
+                                <Form.Control required type="string" placeholder="Enter username" onChange={(e) => (initialUser.username = e.target.value)}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3 input-div" controlId="formPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control required type="password" placeholder="Password" onChange={(e) => {initialUser.password = e.target.value; }}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3 input-div" controlId="formConPassword">
                                 <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control type="password" placeholder=" Confirm Password" />
+                                <Form.Control required type="password" placeholder=" Confirm Password" onChange={(e) => setConPass(e.target.value)}/>
+                                <Form.Control.Feedback type="invalid">
+                                    Passwords do not match
+                                </Form.Control.Feedback>
                             </Form.Group>
 
                             <Button
@@ -97,12 +85,12 @@ export default function Signup() {
                                 style={{width: "350px"}}
                                 variant="primary"
                                 type="submit"
-                                onClick={handleSubmit}
                             >
                                 Submit
                             </Button>
+
                             <div className="text-center mt-3">
-                                <medium>Already have an account?<br></br><a href="/login">Log in</a></medium>
+                                Already have an account?<br/><a href="/login">Log in</a>
                             </div>
                         </Form>
                     </div>
