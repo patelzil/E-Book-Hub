1d44e41727fcf3dcd19dae0da830c4a9a1a68485
zeel khokhariya <73443435+zeelkhokhariya@users.noreply.github.com>
2022-03-02 13:34:42 -0600

    #89 structure the response object for the book

--- Backend/Controller/bookController.js
+++ Backend/Controller/bookController.js
@@ -1,15 +1,39 @@
 const fetch = require("node-fetch");
+const bookModel = require("../models/bookModel");
+
+let getAPI = (req) => {
+    let api_url;
+    if (req.params.hasOwnProperty("bookTitle")) {
+        api_url = `https://www.googleapis.com/books/v1/volumes?q=${req.params.bookTitle}+intitle:${req.params.bookTitle}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`;
+    } else if (req.params.hasOwnProperty("bookPublisher")) {
+        api_url = `https://www.googleapis.com/books/v1/volumes?q=${req.params.bookAuthor}+inauthor:${req.params.bookAuthor}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`;
+    } else if (req.params.hasOwnProperty("bookCategory")) {
+        api_url = `https://www.googleapis.com/books/v1/volumes?q=${req.params.bookCategory}+subject:${req.params.bookCategory}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`;
+    } else if (req.params.hasOwnProperty("bookPublisher")) {
+        api_url = `https://www.googleapis.com/books/v1/volumes?q=${req.params.bookPublisher}+inpublisher:${req.params.bookPublisher}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`;
+    }
+    if (req.params.hasOwnProperty("bookTitleFree")) {
+        api_url = `https://www.googleapis.com/books/v1/volumes?q=${req.params.bookTitleFree}+intitle:${req.params.bookTitleFree}&filter=free-ebooks&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`;
+    } else {
+        api_url = `https://www.googleapis.com/books/v1/volumes?q="comedy"+subject:"comedy"&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`;
+    }
+
+    return api_url;
+};
 
 exports.getBook = async(req, res) => {
+    let api_url = getAPI(req);
+    console.log(req);
     try {
-        const api_url = `https://www.googleapis.com/books/v1/volumes?q=${req.params.bookname}+intitle:${req.params.bookname}&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`;
-
         const fetch_response = await fetch(api_url);
         const fetchObject = await fetch_response.json();
+
+        responseBooks = bookModel.getBooksResponse(fetchObject);
+
         res.status(200).json({
             status: "success",
             data: {
-                fetchObject,
+                responseBooks,
             },
         });
     } catch (err) {
