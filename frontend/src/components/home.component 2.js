092fc272eb2cb443194d14031ba5ac36d8d9f79c
Viki Makadia <viki@Vikis-MacBook-Air.local>
2022-03-02 22:49:30 -0600

    (#88) Homepage - searchbar and filter ui

---
+++ frontend/src/components/home.component 2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+
+        
+
+        return (
+            <>
+            <h1> Welcome Home!</h1>
+            </>
+        )
+    }
--- frontend/src/components/home.component.js
+++ frontend/src/components/home.component.js
@@ -1,12 +1,32 @@
-import React from "react";
+import axios from 'axios';
+import React, { useState } from "react";
+import SearchBar from "./SearchBar";
 
-export default function Home() {
 
-        
+export default function Home(){
+    const [bookList, setBookList] = useState([])
 
-        return (
-            <>
-            <h1> Welcome Home!</h1>
-            </>
-        )
+    //run this functions when search bar is submitted
+    const onSearchSubmit = (searchTerm, filterValue, minPrice, maxPrice) => {
+        console.log("Searchterm: ", searchTerm, " filtervalue:", filterValue, "PriceSet", minPrice,"-", maxPrice);
+        //call to axios function
+       //filterValue : Possible terms are {searchTitle, searchAuthor, searchPrice/searchFree, searchCategory}
+        axios.get('http://localhost:5000/EBookHub/books/${filterValue}/${searchTerm}')
+            .then(function(response){
+                if(response.data.status === "success"){
+                    setBookList(response.data.responseBooks);
+                }
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
     }
+
+    return (
+        <div className="ui container" style={{marginTop: '10px'}}>
+            <SearchBar onSubmit={onSearchSubmit}></SearchBar>
+        </div>
+    );
+    
+}
+
