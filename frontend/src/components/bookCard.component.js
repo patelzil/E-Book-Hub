9066f3dd91656973bd533b99c36560e6753cdb3d
Patelzil <58107277+Patelzil@users.noreply.github.com>
2022-03-03 03:35:50 -0600

    Get books from backend and display them properly

--- frontend/src/components/bookCard.component.js
+++ frontend/src/components/bookCard.component.js
@@ -1,26 +1,64 @@
-import React from "react";
-import {Button, Card} from "react-bootstrap";
+import React, { useState } from "react";
+import {Button, Card } from "react-bootstrap";
 import {Rating} from "@mui/material";
-import logo from '../assets/logo.png'
+import Modal from 'react-bootstrap/Modal'
 
 export default function BookCard(props) {
+    const [show, setShow] = useState(false);
+
+    const handleDetails = () => {
+        setShow(true);
+    }
+
     return (
-        <Card style={{ width: '285px', margin: "15px" }}>
-            <div style={{width: "100%", height: "250px", display: "flex", flexDirection: "column", alignItems: "center",
-                justifyContent: "center"}}>
-                <Card.Img  variant="top" src={logo}/>
-            </div>
-            <Card.Body style={{width: "100%", marginTop: "10px"}}>
-                <Card.Title style={{fontSize: "22px", fontWeight: "bold"}}>{props.bookDetails.title}</Card.Title>
-                <Card.Subtitle style={{fontSize: "20px", fontWeight: "bold"}}>{props.bookDetails.subtitle}</Card.Subtitle>
-                <Card.Text style={{fontSize: "18px"}}>{props.bookDetails.author}</Card.Text>
-                <div style={{fontSize: "18px", fontWeight: "bold"}}>$ 29.99
-                    <div style={{ float: "right"}}>
-                        <Rating name="read-only" value={props.bookDetails.rating} precision={0.5} readOnly />
+        <>
+            <div>
+                <Card style={{ width: '260px', height: '450px', margin: "15px" }}>
+                    <div style={{width: "100%", height: "250px", display: "flex", flexDirection: "column", alignItems: "center",
+                        justifyContent: "center"}}>
+                        <Card.Img  width="260px" height="250px" variant="top" src={props.bookDetails.imageLink.thumbnail}/>
                     </div>
-                </div>
-                <Button className="card button" style={{width: "90%"}}>Details</Button>
-            </Card.Body>
-        </Card>
+                    <Card.Body style={{width: "100%", marginTop: "0px"}}>
+                        <Card.Title className="book-title" style={{fontSize: "20px", fontWeight: "bold"}}>{props.bookDetails.title}</Card.Title>
+                        <Card.Subtitle className="book-title" style={{fontSize: "18px", fontWeight: "bold"}}>{props.bookDetails.subtitle}</Card.Subtitle>
+
+                        <Card.Text className="book-title" style={{fontSize: "18px"}}>
+                            {props.bookDetails.authors[0]}
+                        </Card.Text>
+
+                        <div style={{fontSize: "18px", fontWeight: "bold", margin: "0px"}}>FREE
+                            <div style={{ float: "right"}}>
+                                <Rating name="read-only" value={props.bookDetails.rating} precision={0.5} readOnly />
+                            </div>
+                        </div>
+
+                        <Button className="card button" style={{width: "90%", marginBottom: "5px"}} onClick={handleDetails}>Details</Button>
+
+                    </Card.Body>
+                </Card>
+            </div>
+            <div>
+                <Modal
+                    show={show}
+                    onHide={() => setShow(false)}
+                    fullscreen
+                    centered
+                >
+                    <Modal.Body>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            {props.bookDetails.title}
+                        </Modal.Title>
+                        <h4>{props.bookDetails.subtitle}</h4>
+                        <p>
+                            {props.bookDetails.description}
+                        </p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={() => setShow(false)}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+
+        </>
     )
 }
--- frontend/src/components/home.component.js
+++ frontend/src/components/home.component.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import SearchBar from "./SearchBar";
 import Books from "./books.component";
 import NavBar from "./navbar.component";
@@ -7,31 +7,47 @@ import NavBar from "./navbar.component";
 export default function Home(){
     const [bookList, setBookList] = useState([])
 
-    //run this functions when search bar is submitted
-    const onSearchSubmit = (searchTerm, filterValue, minPrice, maxPrice) => {
-        console.log("Searchterm: ", searchTerm, " filtervalue:", filterValue, "PriceSet", minPrice,"-", maxPrice);
-        //call to axios function
-       //filterValue : Possible terms are {searchTitle, searchAuthor, searchPrice/searchFree, searchCategory}
-        axios.get('http://localhost:5000/EBookHub/books/${filterValue}/${searchTerm}')
+    useEffect(() => {
+        axios.get('http://localhost:5000/EBookHub/books/default')
             .then(function(response){
                 if(response.data.status === "success"){
-                    setBookList(response.data.responseBooks);
+                    setBookList(response.data.data.responseBooks);
+                }else{
+                    setBookList([])
                 }
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }, []);
+
+    //run this functions when search bar is submitted
+    const onSearchSubmit = (searchTerm, filterValue, minPrice, maxPrice) => {
+        //call to axios function
+        //filterValue : Possible terms are {searchTitle, searchAuthor, searchPrice/searchFree, searchCategory}
+        const searchUrl = 'http://localhost:5000/EBookHub/books/' + filterValue + '/' + searchTerm;
+        axios.get(searchUrl)
+            .then(function(response){
+                if(response.data.status === "success"){
+                    setBookList(response.data.data.responseBooks);
+                }
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
     }
 
     return (
         <div>
-        <div style={{ zIndex: 1000, top: 0, position: 'sticky', background: 'black' }}>
-        <NavBar/>
-        </div>
-        <div className="ui container" style={{marginTop: '10px'}}>
-          <SearchBar onSubmit={onSearchSubmit}></SearchBar>
-        </div>
-        <div><Books/></div>
+            <div style={{ zIndex: 1000, top: 0, position: 'sticky', background: 'black' }}>
+                <NavBar/>
+            </div>
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={onSearchSubmit}></SearchBar>
+            </div>
+            <div>
+                <Books list={bookList}/>
+            </div>
         </div>
     )
 }
