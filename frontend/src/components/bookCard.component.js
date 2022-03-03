2966f4db83d19372f088a5ff1d61b31f48113f7d
Patelzil <58107277+Patelzil@users.noreply.github.com>
2022-03-03 01:29:07 -0600

    fixed UI errors and deleted extra duplicate files

--- frontend/src/components/bookCard.component.js
+++ frontend/src/components/bookCard.component.js
@@ -1,22 +1,25 @@
 import React from "react";
 import {Button, Card} from "react-bootstrap";
 import {Rating} from "@mui/material";
+import logo from '../assets/logo.png'
 
 export default function BookCard(props) {
     return (
-        <Card style={{ width: '18rem', margin: "15px" }}>
-            <div style={{width: "285px", height: "285px", display: "flex", flexDirection: "column", alignItems: "center",
+        <Card style={{ width: '285px', margin: "15px" }}>
+            <div style={{width: "100%", height: "250px", display: "flex", flexDirection: "column", alignItems: "center",
                 justifyContent: "center"}}>
-                <Card.Img  variant="top" src={props.bookDetails.imageLinks+"?text=E-book-hub"}/>
+                <Card.Img  variant="top" src={logo}/>
             </div>
-            <Card.Body style={{width: "100%"}}>
+            <Card.Body style={{width: "100%", marginTop: "10px"}}>
                 <Card.Title style={{fontSize: "22px", fontWeight: "bold"}}>{props.bookDetails.title}</Card.Title>
                 <Card.Subtitle style={{fontSize: "20px", fontWeight: "bold"}}>{props.bookDetails.subtitle}</Card.Subtitle>
                 <Card.Text style={{fontSize: "18px"}}>{props.bookDetails.author}</Card.Text>
                 <div style={{fontSize: "18px", fontWeight: "bold"}}>$ 29.99
-                    <div style={{ float: "right"}}><Rating name="read-only" value={props.bookDetails.rating} precision={0.5} readOnly /></div>
+                    <div style={{ float: "right"}}>
+                        <Rating name="read-only" value={props.bookDetails.rating} precision={0.5} readOnly />
+                    </div>
                 </div>
-                <Button className="card button">Details</Button>
+                <Button className="card button" style={{width: "90%"}}>Details</Button>
             </Card.Body>
         </Card>
     )
