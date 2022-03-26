8b9df4b89449af22dcf7c7ea93331b808c18ae8d
Patelzil <58107277+Patelzil@users.noreply.github.com>
2022-03-26 04:15:03 -0500

    Initial setup of socket

--- Backend/app.js
+++ Backend/app.js
@@ -2,14 +2,26 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const cors = require("cors");
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
 const userRouter = require("./routes/userRoutes");
 const bookRouter = require("./routes/bookRoutes");
-const app = express();
+
+// Socket server setup
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3005, () => {
+    console.log('listening on *:3005');
+});
+
+// --- Socket server set up ends ---
+
 //Middlewares - output in console a response status
 app.use(cors());
-
-//Middlewares
-//output in console a response status
 
 //allow to parse between javascript object and Json
 app.use(express.json());
