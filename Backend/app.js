ef67edea058b2752d2db39c9fc5903fecaccaa26
Patelzil <58107277+Patelzil@users.noreply.github.com>
2022-03-26 04:23:48 -0500

    connected backend socket server to react frontend

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io")
+const io = new Server(server)
 
 const userRouter = require("./routes/userRoutes");
 const bookRouter = require("./routes/bookRoutes");
@@ -13,6 +15,13 @@ const bookRouter = require("./routes/bookRoutes");
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', ()=> {
+        console.log('user disconnected')
+    })
+})
 
 server.listen(3005, () => {
     console.log('listening on *:3005');
