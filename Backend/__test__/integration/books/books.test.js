cc9062e782c6b596bd38bf375ee6422819fac90e
Rutu <Rutubarvaliya10@gmail.com>
2022-04-05 15:09:49 -0500

    (#135) Updated few tests for Backend

--- Backend/__test__/integration/books/books.test.js
+++ Backend/__test__/integration/books/books.test.js
@@ -27,114 +27,114 @@ describe('Book get test', ()=>
     })
 })
 
-// describe('Book get test', ()=>
-// {
-//     describe('Get default number of books',() =>
-//     {
-//         test('User got default book page',async () =>{
-//             jest.setTimeout(30000);
-//             const response = await request.get('/EBookHub/books/default');
-//             expect(response).toBeTruthy();
-//         })
-//     })
+describe('Book get test', ()=>
+{
+    describe('Get default number of books',() =>
+    {
+        test('User got default book page',async () =>{
+            jest.setTimeout(30000);
+            const response = await request.get('/EBookHub/books/default');
+            expect(response).toBeTruthy();
+        })
+    })
 
-//     describe('Get books with author name',() =>
-//     {
-//         test('User got filtered books with author name',async () =>{
-//             jest.setTimeout(30000);
-//             const authorName = "William Shakespeare";
-//             const response = await request.get(`/EBookHub/books/searchAuthor/${authorName}`);
-//             const obj = JSON.parse(response.text);             
-//             expect(obj.data.responseBooks[0].authors.toString()).toMatch(/William/);
+    describe('Get books with author name',() =>
+    {
+        test('User got filtered books with author name',async () =>{
+            jest.setTimeout(30000);
+            const authorName = "William Shakespeare";
+            const response = await request.get(`/EBookHub/books/searchAuthor/${authorName}`);
+            const obj = JSON.parse(response.text);             
+            expect(obj.data.responseBooks[0].authors.toString()).toMatch(/William/);
             
-//         })
+        })
 
-//         test('User got filtered books if that author is not exist',async () =>{
-//             jest.setTimeout(30000);
-//             //invalid author name sholud not exists to the database
-//             const authorName = "testiiiiii";
-//             const response = await request.get(`/EBookHub/books/searchAuthor/${authorName}`);
-//             const obj = JSON.parse(response.text);             
-//             expect(obj.status).toBe("fail");
-//         })
+        test('User got filtered books if that author is not exist',async () =>{
+            jest.setTimeout(30000);
+            //invalid author name sholud not exists to the database
+            const authorName = "testiiiiii";
+            const response = await request.get(`/EBookHub/books/searchAuthor/${authorName}`);
+            const obj = JSON.parse(response.text);             
+            expect(obj.status).toBe("fail");
+        })
 
 
-//     })
+    })
 
-//     describe('Get books with title name',() =>
-//     {
-//         test('User get the books by searching title name', async () =>{
-//             jest.setTimeout(30000);
-//             const bookTitle = "java";
-//             const response = await request.get(`/EBookHub/books/searchTitle/${bookTitle}`);
-//             const obj = JSON.parse(response.text);
-//             expect(obj.data.responseBooks[0].title).toMatch(/Java/);
-//         })
+    describe('Get books with title name',() =>
+    {
+        test('User get the books by searching title name', async () =>{
+            jest.setTimeout(30000);
+            const bookTitle = "java";
+            const response = await request.get(`/EBookHub/books/searchTitle/${bookTitle}`);
+            const obj = JSON.parse(response.text);
+            expect(obj.data.responseBooks[0].title).toMatch(/Java/);
+        })
 
-//         test('User can not get books by searching irrelevent title name', async () =>{
-//             jest.setTimeout(30000);
-//             const bookTitle = "testiiii";
-//             const response = await request.get(`/EBookHub/books/searchTitle/${bookTitle}`);
-//             const obj = JSON.parse(response.text);
-//             expect(obj.status).toBe("fail");
-//         })
-//     })
+        test('User can not get books by searching irrelevent title name', async () =>{
+            jest.setTimeout(30000);
+            const bookTitle = "testiiii";
+            const response = await request.get(`/EBookHub/books/searchTitle/${bookTitle}`);
+            const obj = JSON.parse(response.text);
+            expect(obj.status).toBe("fail");
+        })
+    })
 
-//     describe('Get books with Category',() =>
-//     {
-//         test('User get the books by searching title name', async () =>{
-//             jest.setTimeout(30000);
-//             const bookCategory = "Education";
-//             const response = await request.get(`/EBookHub/books/searchCategory/${bookCategory}`);
-//             const obj = JSON.parse(response.text);
-//             expect(obj.data.responseBooks[0].category.toString()).toMatch('Education');
-//         })
+    describe('Get books with Category',() =>
+    {
+        test('User get the books by searching title name', async () =>{
+            jest.setTimeout(30000);
+            const bookCategory = "Education";
+            const response = await request.get(`/EBookHub/books/searchCategory/${bookCategory}`);
+            const obj = JSON.parse(response.text);
+            expect(obj.data.responseBooks[0].category.toString()).toMatch('Education');
+        })
 
-//         test('User can not get the books by searching irrelevent category', async () =>{
-//             jest.setTimeout(30000);
-//             const bookCategory = "testtiiiii";
-//             const response = await request.get(`/EBookHub/books/searchCategory/${bookCategory}`);
-//             const obj = JSON.parse(response.text);
-//             expect(obj.status).toBe("fail");
-//         })
-//     })
+        test('User can not get the books by searching irrelevent category', async () =>{
+            jest.setTimeout(30000);
+            const bookCategory = "testtiiiii";
+            const response = await request.get(`/EBookHub/books/searchCategory/${bookCategory}`);
+            const obj = JSON.parse(response.text);
+            expect(obj.status).toBe("fail");
+        })
+    })
 
-//     describe('Get books with publisher name',() =>
-//     {
-//         test('User get the books by searching publisher name', async () =>{
-//             jest.setTimeout(30000);
-//             const publisherName = "Lion";
-//             const response = await request.get(`/EBookHub/books/searchPublisher/${publisherName}`);
-//             const obj = JSON.parse(response.text);
-//             expect(obj.data.responseBooks[0].publisher).toMatch(/Lion/);
-//         })
+    describe('Get books with publisher name',() =>
+    {
+        test('User get the books by searching publisher name', async () =>{
+            jest.setTimeout(30000);
+            const publisherName = "Lion";
+            const response = await request.get(`/EBookHub/books/searchPublisher/${publisherName}`);
+            const obj = JSON.parse(response.text);
+            expect(obj.data.responseBooks[0].publisher).toMatch(/Lion/);
+        })
 
-//         test('User can not get the books by searching irrelevent publisher name', async () =>{
-//             jest.setTimeout(30000);
-//             const publisherName = "testtiiiii";
-//             const response = await request.get(`/EBookHub/books/searchPublisher/${publisherName}`);
-//             const obj = JSON.parse(response.text);
-//             expect(obj.status).toBe("fail");
-//         })
-//     })
+        test('User can not get the books by searching irrelevent publisher name', async () =>{
+            jest.setTimeout(30000);
+            const publisherName = "testtiiiii";
+            const response = await request.get(`/EBookHub/books/searchPublisher/${publisherName}`);
+            const obj = JSON.parse(response.text);
+            expect(obj.status).toBe("fail");
+        })
+    })
 
-//     describe('Get free books',() =>
-//     {
-//         test('User get the free books by filtering free', async () =>{
-//             jest.setTimeout(30000);
-//             const freeBook = "love";
-//             const response = await request.get(`/EBookHub/books/searchFree/${freeBook}`);
-//             const obj = JSON.parse(response.text);
-//             expect(obj.data.responseBooks[0].price).toMatch('FREE');
-//         })
+    describe('Get free books',() =>
+    {
+        test('User get the free books by filtering free', async () =>{
+            jest.setTimeout(30000);
+            const freeBook = "love";
+            const response = await request.get(`/EBookHub/books/searchFree/${freeBook}`);
+            const obj = JSON.parse(response.text);
+            expect(obj.data.responseBooks[0].price).toMatch('FREE');
+        })
 
-//         test('User can not get the books by searching irrelevent publisher name', async () =>{
-//             jest.setTimeout(30000);
-//             const freeBook = "testtiiiii";
-//             const response = await request.get(`/EBookHub/books/searchFree/${freeBook}`);
-//             const obj = JSON.parse(response.text);
-//             expect(obj.status).toBe("fail");
-//         })
-//     })
+        test('User can not get the books by searching irrelevent publisher name', async () =>{
+            jest.setTimeout(30000);
+            const freeBook = "testtiiiii";
+            const response = await request.get(`/EBookHub/books/searchFree/${freeBook}`);
+            const obj = JSON.parse(response.text);
+            expect(obj.status).toBe("fail");
+        })
+    })
     
-// })
+})
--- Backend/__test__/unit/Controller/book.test.js
+++ Backend/__test__/unit/Controller/book.test.js
@@ -29,135 +29,135 @@ describe('Book controller mock test', () => {
         })
     })
     
-    // test('Retrive book from API by default page', async ()=>{
-    //     jest.setTimeout(30000)
-    //     const req = await mockRequest({
-    //         params:{},
-    //            fetch:`https://www.googleapis.com/books/v1/volumes?q="comedy"+subject:"comedy"&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
-    //         method: 'GET'
-    //     })
-    //     const res = await mockResponse({
-    //         hostname: 'tester',
-    //         status: function (statusCode) {
-    //             this.status = statusCode
-    //         },
-    //         json: function (body) {
-    //             this.json = body
-    //         }
-    //     })
-    //     await getBook(req,res).then( async()=> {
-    //         expect(res.json.status).toBe('success')
-    //         // console.log(res.json.data);
-    //     })
-    // })
+    test('Retrive book from API by default page', async ()=>{
+        jest.setTimeout(30000)
+        const req = await mockRequest({
+            params:{},
+               fetch:`https://www.googleapis.com/books/v1/volumes?q="comedy"+subject:"comedy"&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
+            method: 'GET'
+        })
+        const res = await mockResponse({
+            hostname: 'tester',
+            status: function (statusCode) {
+                this.status = statusCode
+            },
+            json: function (body) {
+                this.json = body
+            }
+        })
+        await getBook(req,res).then( async()=> {
+            expect(res.json.status).toBe('success')
+            // console.log(res.json.data);
+        })
+    })
 
-    // test('Retrive book from API by title page', async ()=>{
-    //     jest.setTimeout(30000)
-    //     const search = 'Java'
-    //     const req = await mockRequest({
-    //         params:{bookTitle:'Java'},
-    //         fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+intitle:${search}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
-    //         method: 'GET'
-    //     })
-    //     const res = await mockResponse({
-    //         hostname: 'tester',
-    //         status: function (statusCode) {
-    //             this.status = statusCode
-    //         },
-    //         json: function (body) {
-    //             this.json = body
-    //         }
-    //     })
-    //     await getBook(req,res).then( async()=> {
-    //         expect(res.json.status).toBe('success')
-    //     })
-    // })
+    test('Retrive book from API by title page', async ()=>{
+        jest.setTimeout(30000)
+        const search = 'Java'
+        const req = await mockRequest({
+            params:{bookTitle:'Java'},
+            fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+intitle:${search}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
+            method: 'GET'
+        })
+        const res = await mockResponse({
+            hostname: 'tester',
+            status: function (statusCode) {
+                this.status = statusCode
+            },
+            json: function (body) {
+                this.json = body
+            }
+        })
+        await getBook(req,res).then( async()=> {
+            expect(res.json.status).toBe('success')
+        })
+    })
 
-    // test('Retrive book from API by author name', async ()=>{
-    //     jest.setTimeout(30000)
-    //     const search = 'William Shakespeare'
-    //     const req = await mockRequest({
-    //         params:{bookAuthor:search},
-    //         fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+inauthor:${search}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
-    //         method: 'GET'
-    //     })
-    //     const res = await mockResponse({
-    //         hostname: 'tester',
-    //         status: function (statusCode) {
-    //             this.status = statusCode
-    //         },
-    //         json: function (body) {
-    //             this.json = body
-    //         }
-    //     })
-    //     await getBook(req,res).then( async()=> {
-    //         expect(res.json.status).toBe('success')
-    //     })
-    // })
+    test('Retrive book from API by author name', async ()=>{
+        jest.setTimeout(30000)
+        const search = 'William Shakespeare'
+        const req = await mockRequest({
+            params:{bookAuthor:search},
+            fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+inauthor:${search}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
+            method: 'GET'
+        })
+        const res = await mockResponse({
+            hostname: 'tester',
+            status: function (statusCode) {
+                this.status = statusCode
+            },
+            json: function (body) {
+                this.json = body
+            }
+        })
+        await getBook(req,res).then( async()=> {
+            expect(res.json.status).toBe('success')
+        })
+    })
 
-    // test('Retrive book from API by catagory of book', async ()=>{
-    //     jest.setTimeout(30000)
-    //     const search = 'Education'
-    //     const req = await mockRequest({
-    //         params:{bookCategory:search},
-    //         fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+subject:${search}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
-    //         method: 'GET'
-    //     })
-    //     const res = await mockResponse({
-    //         hostname: 'tester',
-    //         status: function (statusCode) {
-    //             this.status = statusCode
-    //         },
-    //         json: function (body) {
-    //             this.json = body
-    //         }
-    //     })
-    //     await getBook(req,res).then( async()=> {
-    //         expect(res.json.status).toBe('success')
-    //     })
-    // })
+    test('Retrive book from API by catagory of book', async ()=>{
+        jest.setTimeout(30000)
+        const search = 'Education'
+        const req = await mockRequest({
+            params:{bookCategory:search},
+            fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+subject:${search}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
+            method: 'GET'
+        })
+        const res = await mockResponse({
+            hostname: 'tester',
+            status: function (statusCode) {
+                this.status = statusCode
+            },
+            json: function (body) {
+                this.json = body
+            }
+        })
+        await getBook(req,res).then( async()=> {
+            expect(res.json.status).toBe('success')
+        })
+    })
 
-    // test('Retrive book from API by name of publisher', async ()=>{
-    //     jest.setTimeout(30000)
-    //     const search = 'Lion'
-    //     const req = await mockRequest({
-    //         params:{bookPublisher:search},
-    //         fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+inpublisher:${search}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
-    //         method: 'GET'
-    //     })
-    //     const res = await mockResponse({
-    //         hostname: 'tester',
-    //         status: function (statusCode) {
-    //             this.status = statusCode
-    //         },
-    //         json: function (body) {
-    //             this.json = body
-    //         }
-    //     })
-    //     await getBook(req,res).then( async()=> {
-    //         expect(res.json.status).toBe('success')
-    //     })
-    // })
+    test('Retrive book from API by name of publisher', async ()=>{
+        jest.setTimeout(30000)
+        const search = 'Lion'
+        const req = await mockRequest({
+            params:{bookPublisher:search},
+            fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+inpublisher:${search}&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
+            method: 'GET'
+        })
+        const res = await mockResponse({
+            hostname: 'tester',
+            status: function (statusCode) {
+                this.status = statusCode
+            },
+            json: function (body) {
+                this.json = body
+            }
+        })
+        await getBook(req,res).then( async()=> {
+            expect(res.json.status).toBe('success')
+        })
+    })
 
-    // test('Retrive book from API by applying filter of free', async ()=>{
-    //     jest.setTimeout(30000)
-    //     const search = 'Java'
-    //     const req = await mockRequest({
-    //         params:{bookTitleFree:search},
-    //         fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+intitle:${search}&filter=free-ebooks&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
-    //         method: 'GET'
-    //     })
-    //     const res = await mockResponse({
-    //         hostname: 'tester',
-    //         status: function (statusCode) {
-    //             this.status = statusCode
-    //         },
-    //         json: function (body) {
-    //             this.json = body
-    //         }
-    //     })
-    //     await getBook(req,res).then( async()=> {
-    //         expect(res.json.status).toBe('success')
-    //     })
-    // })
+    test('Retrive book from API by applying filter of free', async ()=>{
+        jest.setTimeout(30000)
+        const search = 'Java'
+        const req = await mockRequest({
+            params:{bookTitleFree:search},
+            fetch:`https://www.googleapis.com/books/v1/volumes?q=${search}+intitle:${search}&filter=free-ebooks&printType=books&maxResults=40&key=AIzaSyC4JQ0rgJkbS1AB828COl6fV_kUxsGX3Ao`,
+            method: 'GET'
+        })
+        const res = await mockResponse({
+            hostname: 'tester',
+            status: function (statusCode) {
+                this.status = statusCode
+            },
+            json: function (body) {
+                this.json = body
+            }
+        })
+        await getBook(req,res).then( async()=> {
+            expect(res.json.status).toBe('success')
+        })
+    })
 })
