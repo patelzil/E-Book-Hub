07cb62aea477bbed84b877dfe8d298cf8435ff17
Parth Patel <patelp24@myumanitoba.ca>
2022-04-05 21:04:16 -0500

    Added code for buy/rent book

--- Backend/Controller/purchaseController.js
+++ Backend/Controller/purchaseController.js
@@ -19,6 +19,7 @@ exports.checkPaymentValidation = async(req, res) => {
                 let bookadded = await boughtBooks.create(req.body);
 
                 res.status(200).json({
+                    status: "success",
                     found: bookadded,
                 });
             } else {
@@ -44,18 +45,19 @@ exports.checkPaymentValidation = async(req, res) => {
 
 exports.getAllBooks = async(req, res) => {
     try {
-        let userExist = await Users.exists({ username: req.body.username });
+        let userExist = await Users.exists({ username: req.query.username });
 
         if (userExist) {
-            let books = await boughtBooks.find({ username: req.body.username });
+            let books = await boughtBooks.find({ username: req.query.username });
 
             res.status(200).json({
+                status: "success",
                 found: books,
             });
         } else {
             res.status(404).json({
                 status: "failed",
-                message: "No such user: " + req.body.username,
+                message: "No such user: " + req.query.username,
             });
         }
     } catch (err) {
--- frontend/src/components/user.component.js
+++ frontend/src/components/user.component.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import UserSessionNavBar from "./usersessionnavbar.component";
+import Modal from 'react-bootstrap/Modal';
+import logo from '../assets/logo.png';
+import axios from 'axios';
+import Books from "./books.component";
+import BookClubEvents from "./bookClubEvents.component";
+import BookClubEventCard from "./bookClubEventCard.component";
 
 export default function User() {
 
     
-    const userObject = JSON.parse(localStorage.getItem('userObject'));
+    const userObject =  (localStorage.getItem('userObject') !== null) ? (JSON.parse(localStorage.getItem('userObject'))) : (null);
+    const userName = JSON.parse(localStorage.getItem('userObject')).username;
+    const [bookList, setBookList] = useState([]);
+    const [bookClubList, setBookClubList] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/EBookHub/books/purchase/boughtBooks/getAll/',{ 
+            params: {
+              username: userName
+            }
+        }
+          ).then(function(response){
+                if(response.data.status === "success"){
+                    setBookList(response.data.found)
+                }else{
+                    setBookList([])
+                }
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }, []);
 
         return (
             <> 
@@ -12,19 +39,16 @@ export default function User() {
             <UserSessionNavBar/>
             </div>
             { userObject !== null ? 
-            (
-            <div title="dashBoardMessage">
-                <h1> Welcome to your dashboard, {userObject.firstName} { userObject.lastName} ! </h1>
-                <p>please add appropriate content</p>
-            </div>
-            ) : (
-                <div>
-                <h1> Error 404! </h1>
-                <p>please login again</p>
-                </div>
-            )
+            ( <div title="dashBoardMessage"> <h1> Welcome to your dashboard, {userObject.firstName} { userObject.lastName} ! </h1> </div> ) : ( <div> <h1> Error 404! </h1> <p>please login again</p> </div> )
             }
-            
+            <h2>Book Owned by you:</h2>
+            <div title='listOfBooks'>
+               {(bookList.length > 0) ? (<Books list={bookList} showBuy={false}/>) : (<><h4>No Book Owned</h4></>)}
+            </div>
+            {/* <h2>Book Clubs:</h2>
+            <div title='listOfBooksClubs'>
+                {userObject.bookclub.map((bookClub)=>(<BookClubEventCard key={bookClub.id} bookClubDetails={bookClub} currentUser={userObject}/>))}
+            </div> */}
             </>
         )
     }
