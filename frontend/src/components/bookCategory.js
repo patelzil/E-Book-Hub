6eb4cfe6d0e825d660e4bac087f52dbb2a7e0637
Patelzil <58107277+Patelzil@users.noreply.github.com>
2022-04-16 04:12:32 -0500

    Added buttons to scroll list of books horizontally

--- frontend/src/components/bookCategory.js
+++ frontend/src/components/bookCategory.js
@@ -1,30 +1,51 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import BookCard from "./bookCard.component";
-import {Carousel} from "react-bootstrap";
+// carousel imports
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 export default function BookCategory(props) {
+    const [width, setWidth] = useState(window.innerWidth)
+
+    useLayoutEffect(() => {
+        function updateSize() {
+            setWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', updateSize);
+        updateSize();
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
+
     return (
         <>
             <h3 style={{paddingTop: "10px", paddingLeft: "10px"}}>{props.title}</h3>
-            {/*<div style={{position: "relative"}}>*/}
-            {/*<Carousel>*/}
-                <div className={"custom-scrollbar"} style={{padding: "0px", display: "flex", flexDirection: "row", overflow: "auto"}} title="bookComponent">
+            <div style={{position: "relative"}}>
+                <div className={"custom-scrollbar"} style={{padding: "0px", display: "flex", flexDirection: "row", overflowX: "scroll"}} title="bookComponent">
                     { props.list === undefined ? (
                         <h3 style={{textAlign: "center", margin: "20px"}}>Search to see books.</h3>
                     ) : props.list.length > 0 ? (
-                        props.list.map((book, index)=><Carousel.Item><BookCard id={index.toString()} key={index} bookDetails={book} showBuy={props.showBuy}/></Carousel.Item>)
+                            <Swiper
+                                slidesPerView={width/275}
+                                spaceBetween={30}
+                                slidesPerGroup={4}
+                                loopFillGroupWithBlank={true}
+                                navigation={true}
+                                modules={[ Navigation]}
+                                className="mySwiper"
+                            >
+                                {props.list.map((book, index) => <SwiperSlide>
+                                        <BookCard id={index.toString()} key={index} bookDetails={book} showBuy={props.showBuy}/>
+                                    </SwiperSlide>)
+                                }
+                            </Swiper>
                     ) : (
                         <h3 style={{textAlign: "center", margin: "20px"}}>No books found. Please try again.</h3>
                     )}
                 </div>
-            {/*</Carousel>*/}
-
-                {/*<button style={{position: "absolute", right: "0px", top: "50%"}}*/}
-                {/*    onClick={onClickRight}*/}
-                {/*>*/}
-                {/*    Right*/}
-                {/*</button>*/}
-            {/*</div>*/}
+            </div>
         </>
     )
 }
